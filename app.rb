d330780681db018e636054d9f541b1d7bd3ff72a
Ken Kunz <ken.kunz@vibes.com>
2011-07-28 14:45:13 -0500

    Changed QR module size to 5 px

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class QRGenerator
     if path.length > 1
       qr_data = Rack::Utils.unescape(path[1..-1])
       qr_code = QREncoder.encode(qr_data)
-      qr_png  = qr_code.png.to_blob
+      qr_png  = qr_code.png(:pixels_per_module => 5).to_blob
     end
     [ 200, {'Content-Type'=>'image/png'}, [ qr_png || '' ] ]
   end
