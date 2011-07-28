0406dade0614894421b73de1f87ef07db923e7ff
Ken Kunz <ken.kunz@vibes.com>
2011-07-28 13:37:20 -0500

    Handle empty paths

--- app.rb
+++ app.rb
@@ -2,10 +2,12 @@ require 'qrencoder'
 
 class QRGenerator
   def call(env)
-    qr_data = env['PATH_INFO'][1..-1]
-    qr_code = QREncoder.encode(qr_data)
-    qr_png  = qr_code.png.to_blob
-
-    [ 200, {'Content-Type'=>'image/png'}, [ qr_png ] ]
+    path = env['PATH_INFO']
+    if path.length > 1
+      qr_data = Rack::Utils.unescape(path[1..-1])
+      qr_code = QREncoder.encode(qr_data)
+      qr_png  = qr_code.png.to_blob
+    end
+    [ 200, {'Content-Type'=>'image/png'}, [ qr_png || '' ] ]
   end
 end
