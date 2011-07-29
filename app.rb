42a25afc377b694f8a505c394303974c2f6dc4f4
Ken Kunz <ken.kunz@vibes.com>
2011-07-28 20:31:21 -0500

    Bucket of changes: * add support for module size path parameter * move QR data to query string * return 404 if no query string included
    Guess I should add some tests now :)

--- app.rb
+++ app.rb
@@ -1,13 +1,52 @@
 require 'qrencoder'
 
 class QRGenerator
+  DEFAULT_MODULE_SIZE = 5
+
   def call(env)
-    path = env['PATH_INFO']
-    if path.length > 1
-      qr_data = Rack::Utils.unescape(path[1..-1])
-      qr_code = QREncoder.encode(qr_data)
-      qr_png  = qr_code.png(:pixels_per_module => 5).to_blob
+    @env = env
+
+    if query_string?
+      [ 200, {'Content-Type'=>'image/png'}, self ]
+    else
+      [ 404, {'Content-Type'=>'text/plain'}, ['404 Not Found'] ]
     end
-    [ 200, {'Content-Type'=>'image/png'}, [ qr_png || '' ] ]
   end
+
+  def each
+    yield qr_code_png.to_blob
+  end
+
+  def path_info
+    @env['PATH_INFO']
+  end
+
+  def query_string
+    @env['QUERY_STRING']
+  end
+
+  def query_string?
+    !query_string.length.zero?
+  end
+
+  def unescaped_query_string
+    Rack::Utils.unescape query_string
+  end
+
+  def module_size_param
+    path_info.split('/')[1].to_i.nonzero?
+  end
+
+  def module_size
+    module_size_param || DEFAULT_MODULE_SIZE
+  end
+
+  def qr_code
+    QREncoder.encode(unescaped_query_string)
+  end
+
+  def qr_code_png
+    qr_code.png(:pixels_per_module => module_size)
+  end
+
 end
